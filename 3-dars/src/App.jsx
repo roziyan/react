c635c09227f8c1228299830b0ca1fdbfdbaa0fd6
Solikha000 <kamolovasolikha3gmail.com>
2025-04-29 18:17:50 +0500

    commit

---
+++ 3-dars/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='contanier card ' >
+        <div className='cards' >
+            <h2>Hello my name is: <span>Roziya</span></h2>
+            <h2 className='h2' >This is the day: <span>29.04.2025</span></h2>
+            <h2 className='h2' >A random number from 0 to 100: </h2>
+            <h5>67</h5>
+        </div>
+    </div>
+  )
+}
+
+export default App        
